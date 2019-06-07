6e9c7874b2e1b057519f2670f8e6ec18872216d9
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2019-06-07 15:53:22 +0100

    First Commit

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -21,5 +22,36 @@ public class FakePersonDataAccessService implements PersonDao {
     @Override
     public List<Person> selectAllPeople() {
         return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+       Optional<Person> personExists = selectPersonById(id);
+       if (personExists.isEmpty()) {
+           return 0;
+       }
+       DB.remove(personExists.get());
+       return 1;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person updateP) {
+        return selectPersonById(id)
+                .map(person -> {
+                    int indexOfPersontoUpdate = DB.indexOf(person);
+                    if (indexOfPersontoUpdate >= 0) {
+                        DB.set(indexOfPersontoUpdate, new Person(id, updateP.getName()));
+                        return 1;
+                    }
+                    return 0;
+                })
+                .orElse(0);
     }
 }
--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PersonService {
@@ -14,7 +16,7 @@ public class PersonService {
     private final PersonDao personDao;
 
     @Autowired
-    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+    public PersonService(@Qualifier("postgres") PersonDao personDao) {
         this.personDao = personDao;
     }
 
@@ -24,5 +26,17 @@ public class PersonService {
 
     public List<Person> getAllPeople() {
         return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id) {
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person updatedPerson) {
+        return personDao.updatePersonById(id, updatedPerson);
     }
 }
